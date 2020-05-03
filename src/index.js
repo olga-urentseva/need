5f0b50038363e93d1943924454e9062271bbddb5
Olga Urentseva <lelyalelya9666@gmail.com>
2020-05-03 20:57:16 +0200

    bootstrapping

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const fastify = require('fastify')({ logger: true });
+const fs = require('fs/promises');
+const path = require('path');
+const fastifyStatic = require('fastify-static');
+
+fastify.register(require('point-of-view'), {
+  engine: {
+    pug: require('pug'),
+  },
+  root: path.join(__dirname, 'views'),
+  viewExt: 'pug',
+});
+
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, '..', 'public'),
+});
+
+// Declare a route
+fastify.get('/', (request, reply) => {
+  reply.view('pages/index', { text: 'text' });
+});
+
+fastify.setNotFoundHandler((request, reply) => {
+  reply.status(404).view('pages/not-found');
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
