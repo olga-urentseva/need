e5b6591e9371ee5bf1956a67b7d7cd7e02727bb1
Olga Urentseva <lelyalelya9666@gmail.com>
2020-06-18 15:29:36 +0200

    feat(askHelp page): counts of characters in textarea

---
+++ public/js/pages/askhelp.js
@@ -0,0 +1,5 @@
+const $characterNumber = document.querySelector('#character-number');
+const $textarea = document.querySelector('#help-description');
+$textarea.addEventListener('keyup', (event) => {
+  $characterNumber.innerHTML = event.target.value.length + ' / 255';
+});
